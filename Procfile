ef9384980089ff709667d376ad89b410c001c63d
Pawel Zwolinski <p.zwolin@gmail.com>
2018-11-22 10:55:07 +0100

    heroku setup

---
+++ Procfile
@@ -0,0 +1,4 @@
+bundle exec rails s
+bundle exec rake db:create
+bundle exec rake db:migrate
+bundle exec rake db:seed
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,10 +83,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => ENV['APP_DOMAIN'],
     :user_name            => ENV['GMAIL_USERNAME'],
     :password             => ENV['GMAIL_PASSWORD'],
-    :authentication       => "plain",
+    :authentication       => "login",
     :enable_starttls_auto => true
   }
 end
